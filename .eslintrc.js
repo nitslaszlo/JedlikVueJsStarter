f62e518d29ad5af1aa91d6ff1d29a4c24e2ef415
nitslaszlo <nitslaszlo@gmail.com>
2018-10-23 19:35:12 +0200

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/recommended", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    quotes: ["error", "double"]
   },
   parserOptions: {
     parser: "typescript-eslint-parser"
