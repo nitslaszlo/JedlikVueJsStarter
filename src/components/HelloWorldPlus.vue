4a92c0cc2a8ef98b14ca6b25f4df116ae9fefca3
nitslaszlo@gmail.com <nitslaszlo@gmail.com>
2018-12-30 10:11:59 +0100

    up

--- src/components/HelloWorldPlus.vue
+++ src/components/HelloWorldPlus.vue
@@ -1,26 +1,20 @@
 <template>
   <div id="app">
-    <p>Kérem a neved: 
-      <input
-        v-model="nev"
-        type = "text"></p>
+    <p>
+      Kérem a neved:
+      <input v-model="nev" type="text">
+    </p>
     <h1>Hello {{ nev }}{{ felkialtojelek }}</h1>
     <p>Felkiáltójelek száma: {{ felkialtojelDarab }}</p>
-    <button
-      :disabled="felkialtojelDarab == 10"
-      @click="OnClick('+')">Plus</button>
-    <button
-      :disabled="felkialtojelDarab == 1"
-      @click="OnClick('-')">Minus</button>
+    <button :disabled="felkialtojelDarab == 10" @click="OnClick('+')">Plus</button>
+    <button :disabled="felkialtojelDarab == 1" @click="OnClick('-')">Minus</button>
     <ol>
-      <li 
-        v-for="nap in napok" 
-        :key="nap">{{ nap }}</li>
+      <li v-for="nap in napok" :key="nap">{{ nap }}</li>
     </ol>
-    <p>Nap: 
-      <input
-        v-model="inputNap"
-        type = "text"></p>
+    <p>
+      Nap:
+      <input v-model="inputNap" type="text">
+    </p>
     <button @click="HozzadNap">Nap hozzáadása</button>
     <button @click="TorolNap">Nap törlése</button>
   </div>
