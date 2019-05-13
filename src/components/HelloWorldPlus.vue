ab53634606ce75b3d046395c8f645f9bf0345a60
nitslaszlo <nitslaszlo@gmail.com>
2019-05-13 13:45:04 +0200

    up

--- src/components/HelloWorldPlus.vue
+++ src/components/HelloWorldPlus.vue
@@ -11,7 +11,8 @@
     <p>Felkiáltójelek száma: {{ felkialtojelDarab }}</p>
     <button type="button" class="btn m-2 btn-success" :disabled="felkialtojelDarab == 10"
       v-on:click="onClick('+')">Plus</button>
-    <button type="button" class="btn btn-danger" :disabled="felkialtojelDarab == 1" @click="onClick('-')">Minus</button>
+    <button type="button" class="btn btn-danger" :disabled="felkialtojelDarab == 1" v-on:click="onClick('-')">Minus
+    </button>
     <ol>
       <li v-for="nap in napok" :key="nap">{{ nap }}</li>
     </ol>
@@ -22,8 +23,10 @@
       <input type="text" class="form-control" aria-label="Default" aria-describedby="ig2" v-model="inputNap">
     </div>
 
-    <button type="button" class="btn mr-2 btn-success" @click="hozzadNap()">Nap hozzáadása</button>
-    <button type="button" class="btn btn-danger" @click="torolNap()">Nap törlése</button>
+    <button type="button" class="btn mr-2 btn-success" :disabled="!joNapHozzadni(iNap)" @click="hozzadNap()">Nap
+      hozzáadása</button>
+    <button type="button" class="btn btn-danger" :disabled="!joNapTorolni(iNap)" @click="torolNap()">Nap
+      törlése</button>
 
   </div>
 </template>
@@ -55,8 +58,8 @@ export default class HelloWorldPlus extends Vue {
     }
   }
 
-  private hozzadNap(): void {
-    const joNap = [
+  private joNapHozzadni(nap: string): boolean {
+    const joNapok = [
       "hétfő",
       "kedd",
       "szerda",
@@ -65,14 +68,20 @@ export default class HelloWorldPlus extends Vue {
       "szombat",
       "vasárnap"
     ];
-    if (!this.napok.includes(this.iNap) && joNap.includes(this.iNap)) {
-      this.napok.push(this.inputNap.toLowerCase());
-    }
+    return joNapok.includes(nap) && !this.napok.includes(nap);
+  }
+
+  private hozzadNap(): void {
+    this.napok.push(this.inputNap.toLowerCase());
     this.inputNap = "";
   }
 
+  private joNapTorolni(nap: string): boolean {
+    return this.napok.includes(nap);
+  }
+
   private torolNap(): void {
-    this.napok = this.napok.filter(i => i !== this.inputNap);
+    this.napok = this.napok.filter(i => i !== this.iNap);
     this.inputNap = "";
   }
 
